5904ac10bf9662f4ecc92d45e2e5b376f2475a0c
fabio-labella <fabio.labella2@gmail.com>
2015-10-13 23:11:57 +0100

    Reorganise init.el

--- init.el
+++ init.el
@@ -1,88 +1,62 @@
-;; ========== Package list ==========
+;; ========== Setup ==========
+
+;; Package list 
 (require 'package)
 (setq package-archives '(("marmalade" . "http://marmalade-repo.org/packages/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
                          ("melpa-stable" . "http://stable.melpa.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")))
 (package-initialize)
-
-
-;; ========== Load PATH from bash ==========
+;; Load PATH from bash
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
 (exec-path-from-shell-copy-env "PS1")
 (exec-path-from-shell-copy-env "CELLAR")
 
 
-;; ========== Inhibit startup message ===========
-(setq inhibit-startup-message t) 
+;; ========== Backup and autosaves ==========
 
-
-;; ========== Disable the toolbar ==========
-(tool-bar-mode -1)
-
-
-;; ========== Use line numbers globally ==========
-(global-linum-mode t)
-
-
-;; ========== Prompt y or n ==========
-;;Prompt y or n instead of yes or no always.
-(fset 'yes-or-no-p 'y-or-n-p)
-
-
-;; =========== Turn off tab character ===========
-;; Emacs normally uses both tabs and spaces to indent lines. If you
-;; prefer, all indentation can be made from spaces only. To request this,
-;; set `indent-tabs-mode' to `nil'. This is a per-buffer variable;
-;; altering the variable affects only the current buffer, but it can be
-;; disabled for all buffers.
-;; Use (setq ...) to set value locally to a buffer
-;; Use (setq-default ...) to set value globally 
-;;
-(setq-default indent-tabs-mode nil) 
-
-
-;; ========== Support Wheel Mouse Scrolling ==========
-(mouse-wheel-mode t)
-
-
-;; ========== Place Backup Files in Specific Directory ==========
 ;; Enable backup files.
 (setq make-backup-files t)
-
-
 ;; Enable versioning with default values (keep five last versions, I think!)
 (setq version-control t)
-
-
 ;; Save all backup file in this directory.
 (setq backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/"))))
-
-
-;; ========== Place Autosave Files in Specific Directory ==========
+;; Autosave Files in Specific directory
 (setq auto-save-file-name-transforms
       `((".*" ,"~/.emacs.d/autosaves/" t)))
 
 
-;; ========== Enable Line and Column Numbering ==========
+
+;; ========== General Appearance and Settings==========
+
+;; Inhibit startup message 
+(setq inhibit-startup-message t) 
+;; Disable the toolbar
+(tool-bar-mode -1)
+;; Colour theme 
+(load-theme 'tango-dark t) 
+;; Use line numbers globally
+(global-linum-mode t)
 ;; Show line-number in the mode line
 (line-number-mode 1)
 ;; Show column-number in the mode line
 (column-number-mode 1)
+;;Prompt y or n instead of yes or no always.
+(fset 'yes-or-no-p 'y-or-n-p)
+;; Use only spaces for indentatin
+(setq-default indent-tabs-mode nil) 
+;;Support Wheel Mouse Scrolling 
+(mouse-wheel-mode t)
 
+;; ========== Core usage ==========
 
-;; ========== Redefine some common keybindings ==========
-;; the following are redefined so that common commands
-;; only require one C-x prefix, that will be bound to
-;; a keychord
-(global-set-key (kbd "C-x f") 'find-file)
-(global-set-key (kbd "C-x C-f") 'set-fill-column)
-(global-set-key (kbd "C-x s") 'save-buffer)
-(global-set-key (kbd "C-x C-s") 'save-some-buffers)
-
-
-;; ========== Ido mode and Smex ==========
+;; Set Right Command as Ctrl 
+(setq mac-right-command-modifier 'control)
+;; Window management
+(global-set-key (kbd "C-x o") 'ace-window)
+(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+(windmove-default-keybindings)
 ;; Enable Ido mode
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
@@ -91,21 +65,21 @@
 ;; Enable Smex
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
-;; This is your old M-x.
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+;; Key chords 
+(require 'key-chord)
+(key-chord-define-global "fk" 'smex)
+(key-chord-define-global "fj" ctl-x-map)
+(key-chord-mode +1)
+;; Redefine common keybindings to work smoothly with keychords
+(global-set-key (kbd "C-x f") 'find-file)
+(global-set-key (kbd "C-x C-f") 'set-fill-column)
+(global-set-key (kbd "C-x s") 'save-buffer)
+(global-set-key (kbd "C-x C-s") 'save-some-buffers)
 
-
-;; ========== Set Right Command as Ctrl ==========
-(setq mac-right-command-modifier 'control)
-
-
-;; ========== Window management ==========
-(global-set-key (kbd "C-x o") 'ace-window)
-(setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
-(windmove-default-keybindings)
 
 
 ;; ========== Haskell mode ==========
+
 (require 'haskell-interactive-mode)
 (require 'haskell-process)
 (add-hook 'haskell-mode-hook 'interactive-haskell-mode)
@@ -115,15 +89,9 @@
 (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
 
 
-;; ========== Colour theme ==========
-(load-theme 'tango-dark t) 
 
 
-;; ========== Key chords ==========
-(require 'key-chord)
-(key-chord-define-global "fk" 'smex)
-(key-chord-define-global "fj" ctl-x-map)
-(key-chord-mode +1)
+
 
 
 
