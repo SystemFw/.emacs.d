bbe9b61720e0ff1ec2b4d90d68e916acde51e99e
fabio labella <fabio.labella@ncr.com>
2015-12-16 12:11:29 +0000

    Delete excess backup versions automatically

--- init.el
+++ init.el
@@ -20,6 +20,8 @@
 (setq make-backup-files t)
 ;; Enable versioning with default values (keep five last versions, I think!)
 (setq version-control t)
+;; Delete old backup versions
+(setq delete-old-versions t)
 ;; Save all backup file in this directory.
 (setq backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/"))))
 ;; Autosave Files in Specific directory
