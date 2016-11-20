17e7895369b76d761ac45e114158b66e4e4b2c95
fabio-labella <fabio.labella2@gmail.com>
2016-11-20 18:41:35 +0000

    Improve configuration structure

--- init.el
+++ init.el
@@ -18,16 +18,13 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
 (eval-when-compile
   (require 'use-package))
 (require 'diminish)
 (require 'bind-key)
-
-;;;;  * Disable clutter
-(setq inhibit-startup-message t
-      inhibit-splash-screen t
-      ring-bell-function 'ignore)
+(use-package use-package-chords ;; define key-chords in use package declarations
+  :ensure key-chord
+  :ensure t)
 
 ;;;;  * Backups, autosaves, and desktop saves
 (let ((backup-dir "backups")
@@ -61,19 +58,22 @@
   (exec-path-from-shell-copy-env "PS1")
   (exec-path-from-shell-copy-env "CELLAR"))
 
-;;;;  * General settings
+;;;;  * Misc settings
 (setq-default indent-tabs-mode nil) ;; Indent with no tabs
 (delete-selection-mode t) ;; Overwrite selected text
 (setq require-final-newline t) ;; Newline at the end of files
 (fset 'yes-or-no-p 'y-or-n-p) ;; Use y or n instead of yes and no
-(mouse-wheel-mode t) ;; Enable scrolling
 (put 'narrow-to-region 'disabled nil) ;; Enable narrowing
 
 ;;;;  * Appearance
+(setq inhibit-startup-message t
+      inhibit-splash-screen t
+      ring-bell-function 'ignore)
 (line-number-mode t) ;; Line numbers in mode line
 (column-number-mode t) ;; Column numbers in mode line
+(mouse-wheel-mode t) ;; Enable scrolling
+(scroll-bar-mode -1) 
 (tool-bar-mode -1)
-(scroll-bar-mode -1)
 (use-package solarized-theme ;; + Blended fringe  - Pervasive shitty pea green
   :ensure t
   :defer t
@@ -106,31 +106,34 @@
   (call-interactively 'load-theme))
 (enable-theme 'planet)
 
-;;;;  * Frame management
-(use-package ace-window ;; quick jump to frames and more
-  :ensure t
-  :bind ("C-x o" . ace-window)
-  :config
-  (setq aw-dispatch-always t)
-  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
-  (setq aw-background nil))
-(winner-mode t) ;; undo-redo frame configuration
-(windmove-default-keybindings) ;; use shift-arrow to move between frames
-
-;;;;  * File, Buffer, and Project Management
-(use-package dired
-  :config
-  (setq dired-dwim-target t) ;; allows copying between two open dired buffers automatically
-  (setq dired-recursive-deletes 'always)
-  (setq dired-recursive-copies 'always)
-  (require 'dired-x)) ;; dired jump to dir of current buffer
-(use-package projectile
+;;;;  * General completion interface
+(use-package ido
   :ensure t
   :config
-  (projectile-mode t))
-(use-package ibuffer
+  (setq ido-enable-flex-matching t
+        ido-everywhere t)
+ (add-to-list 'ido-ignore-buffers "*")
+ (add-to-list 'ido-ignore-files "\\.DS_Store")
+ (ido-mode t))
+(use-package ido-ubiquitous
   :ensure t
-  :bind ("C-x C-b" . ibuffer))
+  :config
+  (ido-ubiquitous-mode t))
+(use-package flx-ido
+  :ensure t
+  :config
+  (setq ido-use-faces nil) ;; disable ido faces to see flx highlights.
+  (flx-ido-mode t))
+(use-package ido-vertical-mode
+  :ensure t
+  :config
+  (setq ido-vertical-define-keys 'C-n-C-p)
+  (ido-vertical-mode t))
+(use-package smex
+  :ensure t
+  :bind (("M-x" . smex)
+         ("M-X" . smex-major-mode-commands))
+  :chords ("fk"  . smex))
 
 ;;;;  * Modal editing
 (use-package key-chord
@@ -144,8 +147,6 @@
   (key-chord-mode t)
   :config
   (key-chord-define-global "fj" ctl-x-map))
-(use-package use-package-chords
-  :ensure t)
 (use-package god-mode
   :ensure t
   :init (require' god-mode-isearch)
@@ -183,35 +184,6 @@
   (setq god-exempt-major-modes nil)
   (setq god-exempt-predicates nil))
 
-;;;;  * General completion interface
-(use-package ido
-  :ensure t
-  :config
-  (setq ido-enable-flex-matching t
-        ido-everywhere t)
- (add-to-list 'ido-ignore-buffers "*")
- (add-to-list 'ido-ignore-files "\\.DS_Store")
- (ido-mode t))
-(use-package ido-ubiquitous
-  :ensure t
-  :config
-  (ido-ubiquitous-mode t))
-(use-package flx-ido
-  :ensure t
-  :config
-  (setq ido-use-faces nil) ;; disable ido faces to see flx highlights.
-  (flx-ido-mode t))
-(use-package ido-vertical-mode
-  :ensure t
-  :config
-  (setq ido-vertical-define-keys 'C-n-C-p)
-  (ido-vertical-mode t))
-(use-package smex
-  :ensure t
-  :bind (("M-x" . smex)
-         ("M-X" . smex-major-mode-commands))
-  :chords ("fk"  . smex))
-
 ;;;;  * Quick movement
 (use-package avy
   :ensure t
@@ -219,6 +191,24 @@
   :config
   (avy-setup-default)
   (customize-set-variable 'avy-timeout-seconds 0.2))
+
+;;;;  * Frame management
+(use-package ace-window ;; quick jump to frames and more
+  :ensure t
+  :bind ("C-x o" . ace-window)
+  :config
+  (setq aw-dispatch-always t)
+  (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
+  (setq aw-background nil))
+(winner-mode t) ;; undo-redo frame configuration
+(windmove-default-keybindings) ;; use shift-arrow to move between frames
+
+;;;;  * Better undo
+(use-package undo-tree
+  :ensure t
+  :bind ("C-x u" . undo-tree-visualize)  ;; undo-tree has this binding in a local
+  :config                                ;; keymap only, causing issues with keychords
+  (global-undo-tree-mode t))
 
 ;;;;  * Autocompletion
 (use-package hippie-exp
@@ -228,6 +218,29 @@
 ;;;;  * Parentheses
 (show-paren-mode t) ;; Show matching parentheses
 (electric-pair-mode t) ;; Insert matching parentheses
+
+;;;;  * File, Buffer, and Project Management
+(use-package dired
+  :config
+  (setq dired-dwim-target t) ;; allows copying between two open dired buffers automatically
+  (setq dired-recursive-deletes 'always)
+  (setq dired-recursive-copies 'always)
+  (require 'dired-x)) ;; dired jump to dir of current buffer
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-mode t))
+(use-package ibuffer
+  :ensure t
+  :bind ("C-x C-b" . ibuffer))
+
+;;;;  * Version control
+(use-package magit ;; Awesome Git porcelain
+  :ensure t)
+
+;;;;  * Rest
+(use-package restclient
+  :ensure t)
 
 ;;;;  * Json
 (use-package json
@@ -240,17 +253,6 @@
 ;;;;  * Markdown
 (use-package markdown-mode
   :ensure t)
-
-;;;;  * Version control
-(use-package magit ;; Awesome Git porcelain
-  :ensure t)
-
-;;;;  * Better undo
-(use-package undo-tree
-  :ensure t
-  :bind ("C-x u" . undo-tree-visualize)  ;; undo-tree has this binding in a local
-  :config                                ;; keymap only, causing issues with keychords
-  (global-undo-tree-mode t))
 
 ;;;;  * Latex
 (use-package tex
@@ -309,7 +311,3 @@
   (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
   (add-hook 'haskell-mode-hook 'haskell-doc-mode)
   :bind ("C-`" . haskell-interactive-bring))
-
-;;;;  * Rest
-(use-package restclient
-  :ensure t)
