70d73d29a7fc70cc72c6ae0d835c204a501bb2a7
fabio labella <fabio.labella@ncr.com>
2016-11-22 17:22:19 +0000

    Refactor for better compliance to style guide

--- init.el
+++ init.el
@@ -1,10 +1,14 @@
-;;;;  * My Emacs configuration
+;;;  * My Emacs configuration
+
 (setq user-full-name "Fabio Labella")
-(add-hook 'emacs-lisp-mode-hook '(lambda ()
-                                   "You can fold this file as an Org buffer"
-                                   (setq-local orgstruct-heading-prefix-regexp ";;;;  ")
-                                   (turn-on-orgstruct)))
-;;;;  * Package sources
+(defun emacs-lisp-fold-file-with-org ()
+  "Init.el can be folded as if it were an Org buffer"
+  (setq-local orgstruct-heading-prefix-regexp ";;;  ")
+  (turn-on-orgstruct))
+(add-hook 'emacs-lisp-mode-hook 'emacs-lisp-fold-file-with-org)
+
+;;;  * Package sources
+
 (require 'package)
 (setq package-enable-at-startup nil)
 (setq package-archives '(("marmalade" . "http://marmalade-repo.org/packages/")
@@ -12,9 +16,10 @@
                          ("melpa-stable" . "http://stable.melpa.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")))
 (package-initialize)
-(setq load-prefer-newer t) ;; don't load outdated bytecode
+(setq load-prefer-newer t) ; don't load outdated bytecode
 
-;;;;  * Set up use-package
+;;;  * Set up use-package
+
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
@@ -22,11 +27,12 @@
   (require 'use-package))
 (require 'diminish)
 (require 'bind-key)
-(use-package use-package-chords ;; define key-chords in use package declarations
+(use-package use-package-chords ; define key-chords in use package declarations
   :ensure key-chord
   :ensure t)
 
-;;;;  * Backups, autosaves, and desktop saves
+;;;  * Backups, autosaves, and desktop saves
+
 (let* ((full-path (lambda (dir-name)
                     (expand-file-name dir-name user-emacs-directory)))
        (create-dir-if-nonexistent (lambda (dir-name)
@@ -46,12 +52,13 @@
     (setq desktop-path `(,desktop-dir))
     (desktop-save-mode -1)))
 
-;;;;  * Mac specific settings
+;;;  * Mac specific settings
+
 (when (eq system-type 'darwin)
   (setq mac-right-command-modifier 'control)
   ;; on a Mac, don't complain about setting variables in .bashrc instead of .bash_profile
   (setq exec-path-from-shell-check-startup-files nil))
-(use-package exec-path-from-shell ;; load path from bash
+(use-package exec-path-from-shell ; load path from bash
   :ensure t
   :if (and (eq system-type 'darwin) (display-graphic-p))
   :config
@@ -59,43 +66,45 @@
   (exec-path-from-shell-copy-env "PS1")
   (exec-path-from-shell-copy-env "CELLAR"))
 
-;;;;  * Misc settings
-(setq-default indent-tabs-mode nil) ;; Indent with no tabs
-(delete-selection-mode t) ;; Overwrite selected text
-(setq require-final-newline t) ;; Newline at the end of files
-(fset 'yes-or-no-p 'y-or-n-p) ;; Use y or n instead of yes and no
-(put 'narrow-to-region 'disabled nil) ;; Enable narrowing
+;;;  * Misc settings
 
-;;;;  * Appearance
+(setq-default indent-tabs-mode nil) ; Indent with no tabs
+(delete-selection-mode t) ; Overwrite selected text
+(setq require-final-newline t) ; Newline at the end of files
+(fset 'yes-or-no-p 'y-or-n-p) ; Use y or n instead of yes and no
+(put 'narrow-to-region 'disabled nil) ; Enable narrowing
+
+;;;  * Appearance
+
 (setq inhibit-startup-message t
       inhibit-splash-screen t
       ring-bell-function 'ignore)
-(line-number-mode t) ;; Line numbers in mode line
-(column-number-mode t) ;; Column numbers in mode line
-(mouse-wheel-mode t) ;; Enable scrolling
+(line-number-mode t) ; Line numbers in mode line
+(column-number-mode t) ; Column numbers in mode line
+(mouse-wheel-mode t) ; Enable scrolling
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
-(use-package solarized-theme ;; + Blended fringe  - Pervasive shitty pea green
+(use-package solarized-theme ; + Blended fringe  - Pervasive shitty pea green
   :ensure t
   :defer t
   :init
   (load-theme 'solarized-dark t t))
-(use-package ample-theme ;; + Low-contrast  - Fringe and some of the colours
+(use-package ample-theme ; + Low-contrast  - Fringe and some of the colours
   :ensure t
   :defer t
   :init
   (load-theme 'ample-flat t t))
-(use-package planet-theme ;; + Very nice colours  - Fringe, awful vertical line.
+(use-package planet-theme ; + Very nice colours  - Fringe, awful vertical line.
   :ensure t
   :defer t
   :init
   (load-theme 'planet t t))
-(use-package subatomic-theme ;; + Nice colours, Blended Fringe and nice vertical line  - Background not nice
+(use-package subatomic-theme ; + Nice colours, Blended Fringe and nice vertical line  - Background not nice
   :ensure t
   :defer t
   :init
   (load-theme 'subatomic t t))
-(use-package twilight-anti-bright-theme ;; + Nice background and colours  - Fringe, weird comment outlining
+(use-package twilight-anti-bright-theme ; + Nice background and colours  - Fringe, weird comment outlining
   :ensure t
   :defer t
   :init
@@ -107,15 +116,16 @@
   (call-interactively 'load-theme))
 (enable-theme 'planet)
 
-;;;;  * General completion interface
+;;;  * General completion interface
+
 (use-package ido
   :ensure t
   :config
   (setq ido-enable-flex-matching t
         ido-everywhere t)
- (add-to-list 'ido-ignore-buffers "*")
- (add-to-list 'ido-ignore-files "\\.DS_Store")
- (ido-mode t))
+  (add-to-list 'ido-ignore-buffers "*")
+  (add-to-list 'ido-ignore-files "\\.DS_Store")
+  (ido-mode t))
 (use-package ido-ubiquitous
   :ensure t
   :config
@@ -123,7 +133,7 @@
 (use-package flx-ido
   :ensure t
   :config
-  (setq ido-use-faces nil) ;; disable ido faces to see flx highlights.
+  (setq ido-use-faces nil) ; disable ido faces to see flx highlights.
   (flx-ido-mode t))
 (use-package ido-vertical-mode
   :ensure t
@@ -136,7 +146,8 @@
          ("M-X" . smex-major-mode-commands))
   :chords ("fk"  . smex))
 
-;;;;  * Modal editing
+;;;  * Modal editing
+
 (use-package key-chord
   :ensure t
   :bind (("C-x f" . find-file)
@@ -185,7 +196,8 @@
   (setq god-exempt-major-modes nil)
   (setq god-exempt-predicates nil))
 
-;;;;  * Quick movement
+;;;  * Quick movement
+
 (use-package avy
   :ensure t
   :chords ("kk" . avy-goto-char-timer)
@@ -193,47 +205,57 @@
   (avy-setup-default)
   (setq avy-timeout-seconds 0.2))
 
-;;;;  * Frame management
-(use-package ace-window ;; quick jump to frames and more
+;;;  * Frame and Window management
+
+(use-package ace-window ; quick jump to frames and more
   :ensure t
   :bind ("C-x o" . ace-window)
   :config
   (setq aw-dispatch-always t)
   (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
   (setq aw-background nil))
-(winner-mode t) ;; undo-redo frame configuration
-(windmove-default-keybindings) ;; use shift-arrow to move between frames
+(winner-mode t) ; undo-redo frame configuration
+(windmove-default-keybindings) ; use shift-arrow to move between frames
 
-;;;;  * Minibuffer
+;;;  * Minibuffer
+
 ;; silence messages in minibuffer when editing it
-;;;;  * Better undo
+
+;;;  * Better undo
+
 (use-package undo-tree
   :ensure t
-  :bind (("C-/" . undo-tree-undo)         ;; undo-tree has these bindings in a local
-         ("C-?" . undo-tree-redo)         ;; keymap only, causing various issues
+  ;; undo-tree has these bindings in a local
+  ;; keymap only, causing various issues    
+  :bind (("C-/" . undo-tree-undo)         
+         ("C-?" . undo-tree-redo)         
          ("C-x u" . undo-tree-visualize))
   :config
   (global-undo-tree-mode t))
 
-;;;;  * Autocompletion
+;;;  * Autocompletion
+
 (use-package hippie-exp
   :ensure t
   :chords ("jj" . hippie-expand))
 
-;;;;  * Parentheses
+;;;  * Parentheses
+
 (use-package smartparens
   :ensure t
   :config
-  (setq sp-highlight-pair-overlay nil) ;; Don't highlight current sexp
+  (setq sp-highlight-pair-overlay nil) ; Don't highlight current sexp
   (smartparens-global-mode t)
   (show-smartparens-global-mode t))
-;;;;  * File, Buffer, and Project Management
+
+;;;  * File, Buffer, and Project Management
+
 (use-package dired
   :config
-  (setq dired-dwim-target t) ;; allows copying between two open dired buffers automatically
+  (setq dired-dwim-target t) ; allows copying between two open dired buffers automatically
   (setq dired-recursive-deletes 'always)
   (setq dired-recursive-copies 'always)
-  (require 'dired-x)) ;; dired jump to dir of current buffer
+  (require 'dired-x)) ; dired jump to dir of current buffer
 (use-package projectile
   :ensure t
   :config
@@ -242,29 +264,35 @@
   :ensure t
   :bind ("C-x C-b" . ibuffer))
 
-;;;;  * Version control
-(use-package magit ;; Awesome Git porcelain
+;;;  * Version control
+
+(use-package magit ; Awesome Git porcelain
   :ensure t)
 
-;;;;  * Rest
+;;;  * Rest
+
 (use-package restclient
   :ensure t)
 
-;;;;  * Json
+;;;  * Json
+
 (use-package json
   :ensure t)
 
-;;;;  * Yaml
+;;;  * Yaml
+
 (use-package yaml-mode
   :ensure t)
 
-;;;;  * Markdown
+;;;  * Markdown
+
 (use-package markdown-mode
   :ensure t)
 
-;;;;  * Latex
+;;;  * Latex
+
 (use-package tex
-  :ensure auctex ;; (use-package auctex :ensure t) does not work with :defer
+  :ensure auctex ; (use-package auctex :ensure t) does not work with :defer
   :bind (:map LaTeX-mode-map
               ("M-p" . latex-preview-pane-start-preview)
               ("C-q" . latex-preview-pane-stop-preview))
@@ -290,7 +318,7 @@
                             (let ((existing-keybindings (get-default keybindings)))
                               (append
                                existing-keybindings
-                               (mapcar (function strip-prefix) existing-keybindings)))))
+                               (mapcar #'strip-prefix existing-keybindings)))))
     (setq LaTeX-font-list (add-stripped 'LaTeX-font-list))
     (setq TeX-font-list (add-stripped 'TeX-font-list))))
 (use-package latex-preview-pane
@@ -306,12 +334,14 @@
     (when (bound-and-true-p latex-preview-pane-mode)
     (latex-preview-pane-mode -1))))
 
-;;;;  * Scala
+;;;  * Scala
+
 (use-package ensime
   :ensure t
   :pin melpa-stable)
 
-;;;;  * Haskell
+;;;  * Haskell
+
 (use-package haskell-mode
   :ensure t
   :config
