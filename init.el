363118ac93d9bb516e2c1ae8602a5b82f4ca095d
fabio labella <fabio.labella@ncr.com>
2016-11-25 10:35:23 +0000

    Add intero-script to ignored buffers
    Related to a bug with ghc on mac. On my machine the bug does not
    happen and the intero-script* buffers are not created either. Might
    revert this if I fix things.

--- init.el
+++ init.el
@@ -139,6 +139,7 @@
   (setq ido-enable-flex-matching t
         ido-everywhere t)
   (add-to-list 'ido-ignore-buffers "*")
+  (add-to-list 'ido-ignore-buffers "intero-script*")
   (add-to-list 'ido-ignore-files "\\.DS_Store")
   (ido-mode t))
 
