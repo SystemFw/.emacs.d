af2bf47d271db956042108ed18ec975271f8e64c
fabio-labella <fabio.labella2@gmail.com>
2017-03-13 18:01:21 +0000

    Add github flavoured markdown to markdown mode

--- init.el
+++ init.el
@@ -317,7 +317,12 @@ displayed in the *Messages* buffer"
 ;;;  * Markdown
 
 (use-package markdown-mode
-  :ensure t)
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
 
 ;;;  * Latex
 
