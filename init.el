fc20096462f0bed5859f6dfdbbbd0ebb396bb38f
Fabio Labella <fabio.labella2@gmail.com>
2022-04-07 03:30:34 +0100

    Install metals (might trim down some things)

--- init.el
+++ init.el
@@ -397,18 +397,79 @@ displayed in the *Messages* buffer"
   :config
   (setq sbt:prefer-nested-projects t))
 
-(use-package ensime-mode
-  :ensure company
-  :ensure popup
-  :load-path "~/projects/ensime-tng-3.0.0/lisp"
-  :commands ensime-mode
-  :hook scala-mode
-  :bind
-  (:map ensime-mode-map
-        ("M-." . ensime-jump-to-definition)
-        ("C-c C-i t" . ensime-type-at-point)
-        ("C-c C-i s" . ensime-symbol-at-point)
-        ("C-c C-r i" . ensime-import-symbol-at-point)))
+;; (use-package ensime-mode
+;;   :ensure company
+;;   :ensure popup
+;;   :load-path "~/projects/ensime-tng-3.0.0/lisp"
+;;   :commands ensime-mode
+;;   :hook scala-mode
+;;   :bind
+;;   (:map ensime-mode-map
+;;         ("M-." . ensime-jump-to-definition)
+;;         ("C-c C-i t" . ensime-type-at-point)
+;;         ("C-c C-i s" . ensime-symbol-at-point)
+;;         ("C-c C-r i" . ensime-import-symbol-at-point)))
+
+;; Enable nice rendering of diagnostics like compile errors.
+(use-package flycheck
+  :ensure t
+  :init (global-flycheck-mode))
+
+(use-package lsp-mode
+  :ensure t
+  ;; Optional - enable lsp-mode automatically in scala files
+  :hook  (scala-mode . lsp)
+         (lsp-mode . lsp-lens-mode)
+  :config
+  ;; Uncomment following section if you would like to tune lsp-mode performance according to
+  ;; https://emacs-lsp.github.io/lsp-mode/page/performance/
+  (setq gc-cons-threshold 100000000) ;; 100mb
+  (setq read-process-output-max (* 1024 1024)) ;; 1mb
+  (setq lsp-idle-delay 0.500)
+  (setq lsp-log-io nil)
+  (setq lsp-completion-provider :capf)
+  (setq lsp-prefer-flymake nil))
+
+;; Add metals backend for lsp-mode
+(use-package lsp-metals
+  :ensure t)
+
+;; Enable nice rendering of documentation on hover
+;;   Warning: on some systems this package can reduce your emacs responsiveness significally.
+;;   (See: https://emacs-lsp.github.io/lsp-mode/page/performance/)
+;;   In that case you have to not only disable this but also remove from the packages since
+;;   lsp-mode can activate it automatically.
+(use-package lsp-ui
+  :ensure t)
+
+;; lsp-mode supports snippets, but in order for them to work you need to use yasnippet
+;; If you don't want to use snippets set lsp-enable-snippet to nil in your lsp-mode settings
+;;   to avoid odd behavior with snippets and indentation
+(use-package yasnippet
+  :ensure t)
+
+;; Use company-capf as a completion provider.
+;;
+;; To Company-lsp users:
+;;   Company-lsp is no longer maintained and has been removed from MELPA.
+;;   Please migrate to company-capf.
+(use-package company
+  :ensure t
+  :hook (scala-mode . company-mode)
+  :config
+  (setq lsp-completion-provider :capf))
+
+;; Use the Debug Adapter Protocol for running tests and debugging
+(use-package posframe
+  :ensure t
+  ;; Posframe is a pop-up tool that must be manually installed for dap-mode
+  )
+(use-package dap-mode
+  :ensure t
+  :hook
+  (lsp-mode . dap-mode)
+  (lsp-mode . dap-ui-mode)
+  )
 
 ;;;  * Haskell
 
