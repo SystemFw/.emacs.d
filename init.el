cb925e85d2e740efb9ea93f1f4be47d77d58cb61
Fabio Labella <fabio.labella2@gmail.com>
2023-01-29 00:14:57 +0000

    Intero doesn't exist anymore

--- init.el
+++ init.el
@@ -105,7 +105,6 @@
   (setq ido-enable-flex-matching t
         ido-everywhere t)
   (add-to-list 'ido-ignore-buffers "*")
-  (add-to-list 'ido-ignore-buffers "intero-script*")
   (add-to-list 'ido-ignore-buffers "magit-*")
   (add-to-list 'ido-ignore-files "\\.DS_Store")
   (ido-mode t)
