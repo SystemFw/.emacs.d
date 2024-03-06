9a91e8548bf8b80334bebe17ca61438d2563f12e
Fabio Labella <fabio.labella2@gmail.com>
2024-03-06 23:39:07 +0000

    Another fix to actually display line numbers

--- init.el
+++ init.el
@@ -46,9 +46,9 @@
       ring-bell-function 'ignore
       cursor-in-non-selected-windows nil)
 
-(global-display-line-numbers-mode t) ; Line numbers in mode line
+(line-number-mode t) ; Line numbers in mode line
 (column-number-mode t) ; Column numbers in mode line
-(display-line-numbers-mode t)
+(global-display-line-numbers-mode t)
 (mouse-wheel-mode t) ; Enable scrolling
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
